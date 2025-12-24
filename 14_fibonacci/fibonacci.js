337d6e43b3a5139c0368b0b9dc1508a6128d0d8c
QinQipeng <126496939+QinQipeng@users.noreply.github.com>
2025-12-24 18:49:39 +0800

    All JavaScript exercises at introductory level are completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
 
+function recur_fib (num){
+    if (num <= 1) return num;
+    return recur_fib(num - 1) + recur_fib(num - 2);
+}
+
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    num = parseInt(num);
+    return recur_fib(num);
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.sort((a, b) => {
+        let thisYear = (new Date()).getFullYear()
+        aLived = (('yearOfDeath' in a) ? a.yearOfDeath : thisYear) - a.yearOfBirth;
+        bLived = (('yearOfDeath' in b) ? b.yearOfDeath : thisYear) - b.yearOfBirth;
+        return bLived - aLived;
+    })[0];
 };
 
 // Do not edit below this line
